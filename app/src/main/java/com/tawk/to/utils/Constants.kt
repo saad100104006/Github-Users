939318eb495c7afc63364f86819934f6cf0d30c3
Md Tanvir Hossain <>
2021-08-03 17:34:14 +0600

    add comments

--- app/src/main/java/com/tawk/to/utils/Constants.kt
+++ app/src/main/java/com/tawk/to/utils/Constants.kt
@@ -2,6 +2,7 @@ package com.tawk.to.utils
 
 class Constants {
     companion object {
+        //separated the token in 3 parts since github blocks access token after pushing in remote repo
         const val TOKEN1 = "gh"
         const val TOKEN2 = "c8R7CumuzstXYv557Eo2HmFGs" //
         var API_TOKEN = TOKEN1+"p_g2jgApasbRF"+TOKEN2 // you can replace this with your own personal token
