0d40d56c55664d9aef820606d7a6e41b9415886b
Md Tanvir Hossain <>
2021-08-03 13:21:27 +0600

    changes of constant file

--- app/src/main/java/com/tawk/to/utils/Constants.kt
+++ app/src/main/java/com/tawk/to/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.tawk.to.utils
 
 class Constants {
     companion object {
-        const val API_TOKEN = "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"
+        const val API_TOKEN = "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"
     }
 
 }
