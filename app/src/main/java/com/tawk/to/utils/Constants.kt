38be5636c58b9202a0c1a153a513fffcf68396e7
Md Tanvir Hossain <>
2021-08-03 13:24:05 +0600

    add comments

--- app/src/main/java/com/tawk/to/utils/Constants.kt
+++ app/src/main/java/com/tawk/to/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.tawk.to.utils
 
 class Constants {
     companion object {
-        const val API_TOKEN = "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"
+        const val API_TOKEN = "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" //replace with your token
     }
 
 }
